8ba5d3b65b654329bf4745b41c16e68ff250a976
JounQin <liujiaren1992@vip.qq.com>
2017-04-14 10:14:21 +0800

    remove unnecessary proxy config due to having been granted permission

--- server/route.js
+++ server/route.js
@@ -2,20 +2,13 @@ import vm from 'vm'
 import axios from 'axios'
 import encoding from 'encoding'
 
-let proxy
-
-try {
-  proxy = require('./proxy').default
-} catch (e) {}
-
 const parseJsonp = data => vm.runInNewContext(`const JsonCallback = v => v; ${encoding.convert(data, 'utf8', 'gbk')}`)
 
 export default router => {
   router.get('/:type(all|new)-songs', async ctx => {
     const url = `http://music.qq.com/musicbox/shop/v3/data/hit/hit_${ctx.url.includes('all') ? 'all' : 'newsong'}.js`
     ctx.body = parseJsonp((await axios.get(url, {
-      responseType: 'arraybuffer',
-      proxy
+      responseType: 'arraybuffer'
     })).data).songlist
   })
 }
